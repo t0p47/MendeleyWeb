02d388f8677771c0bacfde6d68ed1c6fa8cfb395
t0p47 <t0p47@yandex.ru>
2018-02-07 15:27:42 +0700

    01

--- routes/api.php
+++ routes/api.php
@@ -68,7 +68,7 @@ $api->version('v1',['middleware' => 'api.auth'], function($api){
 	//Route::post('view-pdf', 'JournalArticleController@viewPdf');
 	$api->post('download/article','App\Http\Controllers\JournalArticleController@viewPdf');
 
-	$api->post('windows/send/file','App\Http\Controllers\JournalArticleController@receiveWindowsFile');
+	$api->get('windows/send/file','App\Http\Controllers\JournalArticleController@receiveWindowsFile');
 
 	$api->post('android/folder/request','App\Http\Controllers\FolderController@sendRequestBack');
 
